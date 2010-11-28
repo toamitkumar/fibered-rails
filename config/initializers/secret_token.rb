77241bf43a3e332982c8570914452ba244acbefd
root <root@ubuntu.ubuntu-domain>
2010-11-28 13:51:07 -0500

    Adding fibered rails example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiberedRails::Application.config.secret_token = 'a82a3171cfa8a5ae8cca4933c668d90d0e8cf20ab8e9b3e766b052283af444fd2ac8a7ca39594da829242405e3101041cbcf6cc64f05ebd45977f4d812dba3c3'
